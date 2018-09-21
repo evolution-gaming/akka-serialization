68148196939d7a23d7f42d423f78b8c583a5cb0a
Yaroslav Klymko <t3hnar@gmail.com>
2018-09-21 22:29:55 +0200

    improve SerializedMsgConverterSpec

--- src/test/scala/com/evolutiongaming/serialization/SerializedMsgConverterSpec.scala
+++ src/test/scala/com/evolutiongaming/serialization/SerializedMsgConverterSpec.scala
@@ -1,6 +1,7 @@
 package com.evolutiongaming.serialization
 
 import akka.actor.ActorSystem
+import akka.serialization.{SerializationExtension, SerializerWithStringManifest}
 import org.scalatest.{FunSuite, Matchers}
 
 import scala.concurrent.Await
@@ -11,9 +12,15 @@ class SerializedMsgConverterSpec extends FunSuite with Matchers {
   test("toMsg and fromMsg") {
     val system = ActorSystem(getClass.getSimpleName)
     val converter = SerializedMsgExt(system)
+    val serialization = SerializationExtension(system)
     val value = "value"
-    val msg = converter.toMsg(value)
-    converter.fromMsg(msg).get shouldEqual value
+    val expected = converter.toMsg(value)
+    val bytes = serialization.serialize(expected).get
+    val serializer = serialization.findSerializerFor(expected).asInstanceOf[SerializerWithStringManifest]
+    val manifest = serializer.manifest(expected)
+    val identifier = serializer.identifier
+    val actual = serialization.deserialize(bytes, identifier, manifest).get.asInstanceOf[SerializedMsg]
+    converter.fromMsg(actual).get shouldEqual value
     Await.result(system.terminate(), 3.seconds)
   }
 }
