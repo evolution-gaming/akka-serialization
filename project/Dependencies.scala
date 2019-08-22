44cc1c5ca8694d4d3ff3479515c6e30f9d536ffe
Yaroslav Klymko <t3hnar@gmail.com>
2019-08-22 15:00:39 +0300

    bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.7"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
 
   object Akka {
-    private val version = "2.5.21"
+    private val version = "2.5.25"
     val actor = "com.typesafe.akka" %% "akka-actor" % version
   }
 
   object Scodec {
-    val core = "org.scodec" %% "scodec-core" % "1.11.3"
-    val bits = "org.scodec" %% "scodec-bits" % "1.1.9"
+    val core = "org.scodec" %% "scodec-core" % "1.11.4"
+    val bits = "org.scodec" %% "scodec-bits" % "1.1.12"
   }
 }
